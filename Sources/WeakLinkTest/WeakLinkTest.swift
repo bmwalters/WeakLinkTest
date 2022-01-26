b091a09fc10f124befde236fec8070f5bb44730e
Bradley Walters <oss@walters.app>
2022-01-25 23:52:37 -0700

    maybe with a wrapper

--- Sources/WeakLinkTest/WeakLinkTest.swift
+++ Sources/WeakLinkTest/WeakLinkTest.swift
@@ -3,7 +3,18 @@ import CoreNFC
 public struct WeakLinkTest {
     public func doTest() -> Bool {
         if #available(iOS 13.0, *) {
-            // introduced in iOS 11.0, but framework may not be present on some devices!
+            return AvailabilityWrapper().wrappedReadingAvailable
+        } else {
+            return false
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+class AvailabilityWrapper {
+    @available(iOS 13.0, *)
+    var wrappedReadingAvailable: Bool {
+        if #available(iOS 13.0, *) {
             return NFCNDEFReaderSession.readingAvailable
         } else {
             return false
