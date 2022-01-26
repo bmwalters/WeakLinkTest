78e0800728fbadfed9b099543fb35dee7a2219dd
Bradley Walters <oss@walters.app>
2022-01-25 23:52:37 -0700

    maybe if we just reference a highly-annotated symbol

--- Sources/WeakLinkTest/WeakLinkTest.swift
+++ Sources/WeakLinkTest/WeakLinkTest.swift
@@ -1,12 +1,19 @@
 import CoreNFC
 
 public struct WeakLinkTest {
+    // We still need to trust the caller not to use this symbol on
+    // devices without the framework, but at least they should be
+    // able to /think about using it/ without dyld crashes.
     public func doTest() -> Bool {
-        if #available(iOS 13.0, *) {
-            // introduced in iOS 11.0, but framework may not be present on some devices!
-            return NFCNDEFReaderSession.readingAvailable
-        } else {
-            return false
+        if Date().timeIntervalSince1970 == TimeInterval.greatestFiniteMagnitude {
+            // This will never be hit, but NFCTagReaderSession is iOS 13.0+.
+            // Even if we have enclosing iOS 13.0+ availability modifiers, if we do not use
+            // any iOS-13.0+-annotated symbols from the framework then it will still
+            // be strongly linked.
+            if #available(iOS 13.0, *) {
+                print(NFCTagReaderSession.readingAvailable)
+            }
         }
+        return NFCNDEFReaderSession.readingAvailable
     }
 }
