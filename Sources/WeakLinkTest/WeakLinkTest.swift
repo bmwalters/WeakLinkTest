9ee951d65eb1543f5685a73cb555c6bf9cc5298b
Bradley Walters <oss@walters.app>
2022-01-26 00:08:05 -0700

    maybe if all uses were guarded

--- Sources/WeakLinkTest/WeakLinkTest.swift
+++ Sources/WeakLinkTest/WeakLinkTest.swift
@@ -5,15 +5,17 @@ public struct WeakLinkTest {
     // devices without the framework, but at least they should be
     // able to /think about using it/ without dyld crashes.
     public func doTest() -> Bool {
-        if Date().timeIntervalSince1970 == TimeInterval.greatestFiniteMagnitude {
-            // This will never be hit, but NFCTagReaderSession is iOS 13.0+.
-            // Even if we have enclosing iOS 13.0+ availability modifiers, if we do not use
-            // any iOS-13.0+-annotated symbols from the framework then it will still
-            // be strongly linked.
-            if #available(iOS 13.0, *) {
+        if #available(iOS 13.0, *) {
+            if Date().timeIntervalSince1970 == TimeInterval.greatestFiniteMagnitude {
+                // This will never be hit, but NFCTagReaderSession is iOS 13.0+.
+                // Even if we have enclosing iOS 13.0+ availability modifiers, if we do not use
+                // any iOS-13.0+-annotated symbols from the framework then it will still
+                // be strongly linked.
                 print(NFCTagReaderSession.readingAvailable)
             }
+            return NFCNDEFReaderSession.readingAvailable
+        } else {
+            return false
         }
-        return NFCNDEFReaderSession.readingAvailable
     }
 }
